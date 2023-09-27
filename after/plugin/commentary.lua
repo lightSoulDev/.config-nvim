7427d502f9675a7296adc38d8003efbcccbba530
frost0077 <nurzhan.k@lightsoul.dev>
2023-09-28 05:34:10 +0600

    remaps & harpoon

--- after/plugin/commentary.lua
+++ after/plugin/commentary.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<C-_>", vim.cmd.Commentary)
-vim.keymap.set("i", "<C-_>", vim.cmd.Commentary)
+vim.keymap.set("n", "<C-/>", vim.cmd.Commentary)
+vim.keymap.set("i", "<C-/>", vim.cmd.Commentary)
 -- or <C-/> it depends on your keyboard layout
--- lua/lightsoul/remap.lua
+++ lua/lightsoul/remap.lua
@@ -9,3 +9,9 @@ map("n", "<C-y>", "<C-r>", opts)
 
 map("i", "<C-z>", "<C-o>u", opts)
 map("i", "<C-y>", "<C-o><C-r>", opts)
+-- Ctrl + Shift + arrows to move between splits
+map("n", "<C-S-Left>", "<C-w>h", opts)
+map("n", "<C-S-Down>", "<C-w>j", opts)
+map("n", "<C-S-Up>", "<C-w>k", opts)
+map("n", "<C-S-Right>", "<C-w>l", opts)
+map("n", "<leader><Tab>", ":b#<CR>", opts)
