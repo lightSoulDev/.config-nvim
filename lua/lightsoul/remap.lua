205f9f6dc68ebe4af8d9d1b0d8591a89d0963ba1
frost0077 <nurzhan.k@lightsoul.dev>
2023-09-28 07:36:50 +0600

    move splits

--- lua/lightsoul/remap.lua
+++ lua/lightsoul/remap.lua
@@ -6,6 +6,7 @@ local opts = { noremap = true, silent = true }
 -- let g:ftplugin_sql_omni_key = '<C-j>' IN LUA
 vim.g.ftplugin_sql_omni_key = "<C-j>"
 
+
 map("n", "Q", "<nop>", opts)
 -- map("n", "<leader>e", ":NvimTreeFindRootDirectory<CR>", opts)
 map("n", "<C-z>", "u", opts)
@@ -18,6 +19,12 @@ map("n", "<C-S-Left>", "<C-w>h", opts)
 map("n", "<C-S-Down>", "<C-w>j", opts)
 map("n", "<C-S-Up>", "<C-w>k", opts)
 map("n", "<C-S-Right>", "<C-w>l", opts)
+
+map("n", "<leader><S-Left>", "<C-w>H", opts)
+map("n", "<leader><S-Down>", "<C-w>J", opts)
+map("n", "<leader><S-Up>", "<C-w>K", opts)
+map("n", "<leader><S-Right>", "<C-w>L", opts)
+
 map("n", "<leader><Tab>", ":b#<CR>", opts)
 map("n", "<leader>q", "<C-w>c", opts)
 map("n", "<leader>Q", "<C-w>o", opts)
@@ -25,6 +32,7 @@ map("n", "<leader>p", "", opts)
 map("v", "<A-Up>", ":m '<-2<CR>gv=gv", opts)
 map("v", "<A-Down>", ":m '>+1<CR>gv=gv", opts)
 
+map("v", "<leader>d", '\"_d', opts)
 map("n", "<leader>y", '\"+y', opts)
 map("v", "<leader>y", '\"+y', opts)
 map("n", "<leader>Y", '\"+Y', opts)
