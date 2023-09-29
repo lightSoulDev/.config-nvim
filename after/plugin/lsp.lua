a3ae3c94df870c9e1630f81fa3e7173b18b517a6
lightsoul <nurzhan.k@lightsoul.dev>
2023-09-29 12:31:08 +0600

    lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -71,6 +71,10 @@ lsp.on_attach(function(client, bufnr)
     })
 
     vim.keymap.set("n", "<leader>gl", "<cmd>Telescope diagnostics<cr>", {buffer=0})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
 end)
 
 
