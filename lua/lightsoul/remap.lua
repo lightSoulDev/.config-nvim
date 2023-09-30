09115d87f729d5c376a854989b2a07e6c8bdc47a
lightsoul <nurzhan.k@lightsoul.dev>
2023-09-30 10:46:35 +0600

    macos § remap

--- lua/lightsoul/remap.lua
+++ lua/lightsoul/remap.lua
@@ -59,5 +59,5 @@ vim.keymap.set("n", "<leader>f", function()
 end)
 
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>`", ":DBUIToggle<CR>")
+vim.keymap.set("n", "<leader>§", ":DBUIToggle<CR>")
 
